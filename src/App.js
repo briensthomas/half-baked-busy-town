33589e9d7b39161aa673c0d921a825b6a383afd8
Brien Thomas <thomasbrien94@gmail.com>
2022-06-09 15:40:25 -0700

    test successful, netlify being dumb

--- src/App.js
+++ src/App.js
@@ -13,6 +13,30 @@ function App() {
   const [lightColor, setLightColor] = useState('red', 'yellow', 'green');
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
   const [vehicles, setVehicles] = useState(['car', 'truck']);
+
+  function handleCarClick() {
+    vehicles.push('car');
+
+    setVehicles(vehicles.slice());
+  }
+
+  function handleBusClick() {
+    vehicles.push('bus');
+
+    setVehicles(vehicles.slice());
+  }
+
+  function handleTruckClick() {
+    vehicles.push('truck');
+
+    setVehicles(vehicles.slice());
+  }
+
+  function handleMotorcycleClick() {
+    vehicles.push('motorcycle');
+
+    setVehicles(vehicles.slice());
+  }
   return (
     <div className="App">
       <div className="fight">
@@ -49,7 +73,6 @@ function App() {
       <div className="buttons">
         {/* when you click this button, the color of the light in state should be set to 'red' */}
         <button onClick={() => setLightColor('red')}>Red</button>
-        
         {/* when you click this button, the color of the light in state should be set to 'yellow' */}
         <button onClick={() => setLightColor('yellow')}>Yellow</button>
         {/* when you click this button, the color of the light in state should be set to 'green' */}
@@ -60,17 +83,17 @@ function App() {
       This prop should be an array of strings like ['car', 'truck', 'truck', 'car', 'bus'].
       Do you have something like that in state that you could pass as a vehicles prop? 
       */}
-      <VehicleList />
+      <VehicleList vehicles={vehicles}/>
       <div className="buttons">
         {/* This part is weird */}
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
-        <button>Car</button>
+        <button onClick={handleCarClick}>Car</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'bus' to the end */}
-        <button>Bus</button>
+        <button onClick={handleBusClick}>Bus</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'truck' to the end */}
-        <button>Truck</button>
+        <button onClick={handleTruckClick}>Truck</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'motorcycle' to the end */}
-        <button>Motorcycle</button>
+        <button onClick={handleMotorcycleClick}>Motorcycle</button>
       </div>
     </div>
   );
