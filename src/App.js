9301d7ca5093a999f40c7e69804669350ab0f838
Brien Thomas <thomasbrien94@gmail.com>
2022-06-09 15:04:19 -0700

    traffic lights default to red, and change class of on with an onClick function

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   // lizardSize should be a number that starts out as 10
   const [lizardSize, setLizardSize] = useState(10);
   // lightColor should be a string that starts out as 'red'
+  const [lightColor, setLightColor] = useState('red', 'yellow', 'green');
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
-
-
+  const [vehicles, setVehicles] = useState(['car', 'truck']);
   return (
     <div className="App">
       <div className="fight">
@@ -25,7 +25,9 @@ function App() {
               Oh no! The alien is gobblin up all the electricity!
             </button>
             {/* when you click this button, the lizard's size in state should go down by one */}
-            <button onClick={() => setLizardSize(lizardSize - 1)}>Amazing! The alien zapped the lizard!</button>
+            <button onClick={() => setLizardSize(lizardSize - 1)}>
+              Amazing! The alien zapped the lizard!
+            </button>
           </div>
         </div>
         <div className="monster">
@@ -33,20 +35,25 @@ function App() {
           <img src="lizard.png" width={lizardSize * 10} />
           <div className="buttons">
             {/* when you click this button, the lizard's size in state should go up by one */}
-            <button onClick={() => setLizardSize(lizardSize + 1)}>Yegads! The lizard is ramping up to its final form!</button>
+            <button onClick={() => setLizardSize(lizardSize + 1)}>
+              Yegads! The lizard is ramping up to its final form!
+            </button>
             {/* when you click this button, the alien's size in state should go down by one */}
-            <button onClick={() => setAlienSize(alienSize - 1)}>Oh my! The lizard chomped down on the alien!</button>
+            <button onClick={() => setAlienSize(alienSize - 1)}>
+              Oh my! The lizard chomped down on the alien!
+            </button>
           </div>
         </div>
       </div>
-      {/* <TrafficLight color={lightColor} /> */}
+      <TrafficLight color={lightColor} />
       <div className="buttons">
         {/* when you click this button, the color of the light in state should be set to 'red' */}
-        <button>Red</button>
+        <button onClick={() => setLightColor('red')}>Red</button>
+        
         {/* when you click this button, the color of the light in state should be set to 'yellow' */}
-        <button>Yellow</button>
+        <button onClick={() => setLightColor('yellow')}>Yellow</button>
         {/* when you click this button, the color of the light in state should be set to 'green' */}
-        <button>Green</button>
+        <button onClick={() => setLightColor('green')}>Green</button>
       </div>
       {/* 
       the VehicleList component takes in one prop: vehicles.
